6118cbb3159263557b3b9f28dc2a85ced3213cf0
adarshisgg <30741380+adarshisgg@users.noreply.github.com>
2017-08-05 10:38:32 +0530

    4th.py

---
+++ 4th.py
@@ -0,0 +1,7 @@
+print " enter the character"
+l= raw_input()
+if l in ('a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print" the character is a letter"
+else :
+    print " the characte is not a letter"
+    
