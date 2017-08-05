a7404010da46fdaef782df7ebba6f81b4d48362d
adarshisgg <30741380+adarshisgg@users.noreply.github.com>
2017-08-05 10:51:52 +0530

    5th.py

---
+++ 5th.py
@@ -0,0 +1,11 @@
+print " enter the numbers"
+c= input()
+b= input ()
+a= input() 
+if a>b and a>c:
+    print a 
+elif b>a and b>c:
+    print b
+else :
+    print c 
+print " is the greatest number"
